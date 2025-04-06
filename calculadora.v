b0084d6112975dbfa870598db57e466da1719146
Matheus de Alencar <matheus.alencar2000@hotmail.com>
2025-04-06 09:21:10 -0300

    Trabalho finalizado

--- calculadora.v
+++ calculadora.v
@@ -5,6 +5,20 @@ module calculadora(
   output  [7:0] saida
 );
 
-// insira seu código aqui
+  // insira seu código aqui
+  reg [7:0] _saida; 
+
+  always @(codigo or entrada_A or entrada_B) begin
+    case (codigo)
+      3'b000:  _saida = 0;                     // Zerar
+      3'b001:  _saida = entrada_A;             // mostrar_a
+      3'b010:  _saida = entrada_B;             // mostrar_b
+      3'b011:  _saida = entrada_A + entrada_B; // somar
+      3'b100:  _saida = entrada_A - entrada_B; // subtrair
+      default: _saida = 0; // inválida
+    endcase
+  end
+
+  assign saida = _saida;
 
 endmodule
