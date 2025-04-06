a4efd0f80b3e785d66cb2675e81550500b862aed
Matheus de Alencar <matheus.alencar2000@hotmail.com>
2025-04-06 09:39:56 -0300

    Trabalho finalizado

--- calculadora.v
+++ calculadora.v
@@ -6,19 +6,35 @@ module calculadora(
 );
 
   // insira seu código aqui
-  reg [7:0] _saida; 
 
-  always @(codigo or entrada_A or entrada_B) begin
-    case (codigo)
-      3'b000:  _saida = 0;                     // Zerar
-      3'b001:  _saida = entrada_A;             // mostrar_a
-      3'b010:  _saida = entrada_B;             // mostrar_b
-      3'b011:  _saida = entrada_A + entrada_B; // somar
-      3'b100:  _saida = entrada_A - entrada_B; // subtrair
-      default: _saida = 0; // inválida
-    endcase
-  end
+  // INICIO FORMA 1 (USANDO REG :/ )
 
-  assign saida = _saida;
+  // reg [7:0] _saida;
+
+  // always @(codigo or entrada_A or entrada_B) begin
+  //   case (codigo)
+  //     3'b000:  _saida = 8'b0;                  // Zerar
+  //     3'b001:  _saida = entrada_A;             // mostrar_a
+  //     3'b010:  _saida = entrada_B;             // mostrar_b
+  //     3'b011:  _saida = entrada_A + entrada_B; // somar
+  //     3'b100:  _saida = entrada_A - entrada_B; // subtrair
+  //     default: _saida = 8'b0;                  // inválida
+  //   endcase
+  // end
+
+  // assign saida = _saida;
+
+  // FIM FORMA 1
+
+  // INICIO FORMA 2
+
+  assign saida = (codigo == 3'b000) ? 8'b0                  : // Zerar
+                 (codigo == 3'b001) ? entrada_A             : // mostrar_a
+                 (codigo == 3'b010) ? entrada_B             : // mostrar_b
+                 (codigo == 3'b011) ? entrada_A + entrada_B : // somar
+                 (codigo == 3'b100) ? entrada_A - entrada_B : // subtrair
+                 8'b0;                                        // inválida
+
+  // FIM FORMA 2 
 
 endmodule
